75d5be6e331ce71d47a51ab44e1b2cd94ec63c74
Andrei <andreisva2023@gmail.com>
2023-03-13 15:12:06 -0700

    fix camera zoom

--- continuity/main.py
+++ continuity/main.py
@@ -8,30 +8,21 @@ def main():
     screen = pygame.display.set_mode([500, 500])
 
     main_renderer = renderer.Renderer(screen, 123)
-    
     running = True
     
     while running:
         for event in pygame.event.get():
-            match event.type:
-                case pygame.QUIT:
-                    running = False
-                case pygame.MOUSEWHEEL:
-                    if event.y > 0:
-                        main_renderer.camera.scale *= 1.5
-                    else:
-                        main_renderer.camera.scale /= 1.5
-                    x, y = pygame.mouse.get_pos()
-                    main_renderer.camera.xpos += x
-                    main_renderer.camera.ypos += y 
-                    print(main_renderer.camera.scale)
-                    
-        if pygame.mouse.get_pressed()[0]:
-            mouse_dx, mouse_dy = pygame.mouse.get_rel()
-            main_renderer.camera.xpos += mouse_dx
-            main_renderer.camera.ypos += mouse_dy
-
-        pygame.mouse.get_rel()
+           if event.type == pygame.QUIT:
+                running = False
+           elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 4:
+                    main_renderer.camera.scale *= 1.5
+                elif event.button == 5:
+                    main_renderer.camera.scale /= 1.5
+           elif event.type == pygame.MOUSEMOTION and pygame.mouse.get_pressed()[2]:
+                mouse_dx, mouse_dy = event.rel
+                main_renderer.camera.xpos += mouse_dx
+                main_renderer.camera.ypos += mouse_dy
                 
         main_renderer.render_screen()
         pygame.display.flip()
--- continuity/renderer.py
+++ continuity/renderer.py
@@ -4,7 +4,7 @@ class Camera:
     ypos: float
     xpos: float
     scale: float
-    def __init__(self, xpos=0.0, ypos=0.0, scale=30):
+    def __init__(self, xpos=0.0, ypos=0.0, scale=1):
         self.xpos = ypos
         self.ypos = ypos
         self.scale = scale
@@ -16,13 +16,19 @@ class Renderer:
         self.screen.fill((0, 0, 255))
         self.terrain = pygame.Surface((50,50))
         self.terrain.fill((0, 255, 0))
+
+        self.other = pygame.Surface((100,100))
+        self.other.fill((255, 0, 0))
         
     def render_screen(self):
         # clear the screen
         self.screen.fill((30,30,30))
-        rect = self.terrain.get_rect()
-        self.terrain = pygame.transform.scale(self.terrain, (self.camera.scale, self.camera.scale))
-        self.screen.blit(self.terrain, pygame.Rect(self.camera.xpos + 250 - self.camera.scale / 2, self.camera.ypos + 250 - self.camera.scale / 2, self.camera.scale, self.camera.scale))
 
+        self.terrain = pygame.transform.scale(self.terrain, (50 * self.camera.scale, 50 * self.camera.scale))
+        self.screen.blit(self.terrain, pygame.Rect((0 + self.camera.xpos) * self.camera.scale, (0 + self.camera.ypos) * self.camera.scale, 50 * self.camera.scale, 50 * self.camera.scale))
+
+        self.other = pygame.transform.scale(self.other, (100 * self.camera.scale, 100 * self.camera.scale))
+        self.screen.blit(self.other, pygame.Rect((100 + self.camera.xpos) * self.camera.scale, (100 + self.camera.ypos) * self.camera.scale, 100 * self.camera.scale, 100 * self.camera.scale))
+        
 def read_heightmap(path):
     pass
