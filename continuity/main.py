43a9cef7049a7730558c2a6c2b70f3ea6df35e3d
Andrei <andreisva2023@gmail.com>
2023-03-14 12:55:32 -0700

    refactor rendering code and add scaling optimization

--- continuity/main.py
+++ continuity/main.py
@@ -6,26 +6,5 @@ import continuity.renderer as renderer
 def main():
     pygame.init()
     screen = pygame.display.set_mode([500, 500])
-
     main_renderer = renderer.Renderer(screen, 123)
-    running = True
-    
-    while running:
-        for event in pygame.event.get():
-           if event.type == pygame.QUIT:
-                running = False
-           elif event.type == pygame.MOUSEBUTTONDOWN:
-               x, y = pygame.mouse.get_pos()
-               if event.button == 4:
-                   main_renderer.camera.scale *= 1.5
-               elif event.button == 5:
-                   main_renderer.camera.scale /= 1.5
-           elif event.type == pygame.MOUSEMOTION and pygame.mouse.get_pressed()[2]:
-                mouse_dx, mouse_dy = event.rel
-                main_renderer.camera.xpos += mouse_dx / main_renderer.camera.scale
-                main_renderer.camera.ypos += mouse_dy / main_renderer.camera.scale
-                
-        main_renderer.render_screen()
-        pygame.display.flip()
-        
-pygame.quit()
+    main_renderer.main_loop()
--- continuity/renderer.py
+++ continuity/renderer.py
@@ -1,4 +1,5 @@
 import pygame
+from dataclasses import dataclass
 
 class Camera:
     ypos: float
@@ -8,26 +9,55 @@ class Camera:
         self.xpos = ypos
         self.ypos = ypos
         self.scale = scale
+
+@dataclass
+class RendererObject:
+    surface: pygame.Surface
+    scale: float
     
 class Renderer:
     def __init__(self, screen, terrain):
         self.camera = Camera()
+
         self.screen = screen
-        self.screen.fill((0, 0, 255))
-        self.terrain = pygame.Surface((500,500))
-        self.terrain.fill((50, 100, 0))
+        
+        self.terrain = RendererObject(pygame.Surface((1000,1000)), 1.0)
+        self.terrain.surface.fill((50, 100, 0))
         
     def render_screen(self):
         # clear the screen
         self.screen.fill((30,30,30))
 
-        self.terrain = pygame.transform.scale(self.terrain, (500 * self.camera.scale, 500 * self.camera.scale))
-        self.screen.blit(self.terrain, pygame.Rect((0 + self.camera.xpos) * self.camera.scale, (0 + self.camera.ypos) * self.camera.scale, 500 * self.camera.scale, 500 * self.camera.scale))
+        if self.terrain.scale != self.camera.scale:
+            self.terrain.surface = pygame.transform.scale(self.terrain.surface, (1000 * self.camera.scale, 1000 * self.camera.scale))
+            self.terrain.scale = self.camera.scale
+        self.screen.blit(self.terrain.surface, pygame.Rect((0 + self.camera.xpos) * self.camera.scale, (0 + self.camera.ypos) * self.camera.scale, 1000 * self.camera.scale, 1000 * self.camera.scale))
 
         pygame.draw.circle(self.screen, (255, 0, 0), ((250 + self.camera.xpos) * self.camera.scale, (250 + self.camera.ypos) * self.camera.scale), 10 * self.camera.scale)
 
-        #self.other = pygame.transform.scale(self.other, (100 * self.camera.scale, 100 * self.camera.scale))
-        #self.screen.blit(self.other, pygame.Rect((100 + self.camera.xpos) * self.camera.scale, (100 + self.camera.ypos) * self.camera.scale, 100 * self.camera.scale, 100 * self.camera.scale))
+
+    def main_loop(self):
+        running = True
+    
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = pygame.mouse.get_pos()
+                    if event.button == 4:
+                        self.camera.scale *= 1.5
+                    elif event.button == 5:
+                        self.camera.scale /= 1.5
+                elif event.type == pygame.MOUSEMOTION and pygame.mouse.get_pressed()[2]:
+                    mouse_dx, mouse_dy = event.rel
+                    self.camera.xpos += mouse_dx / self.camera.scale
+                    self.camera.ypos += mouse_dy / self.camera.scale
+                
+            self.render_screen()
+            pygame.display.flip()
+
+        pygame.quit()
         
 def read_heightmap(path):
     pass
