5581ace85128fed679731a9dbdbfa36204e8f121
Andrei <andreisva2023@gmail.com>
2023-03-19 12:30:53 -0700

    add start of neural network code

---
+++ continuity/network.py
@@ -0,0 +1,16 @@
+import numpy
+from dataclasses import dataclass
+
+def activation(x):
+    if x < 0:
+        return 0
+    else:
+        return x
+
+@dataclass
+class Neuron:
+    activation: float
+    connections: list
+
+def feedforward(network):
+    pass
