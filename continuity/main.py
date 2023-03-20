24b2c782e121e93c8713bffbbfd9e00e4ed24a2c
Andrei <andreisva2023@gmail.com>
2023-03-19 19:27:03 -0700

    remove camera movement
    this added too much complexity for too little benefit

--- continuity/main.py
+++ continuity/main.py
@@ -2,9 +2,10 @@ import pygame
 # import numpy
 
 import continuity.renderer as renderer
+import continuity.network as network
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode([500, 500])
-    main_renderer = renderer.Renderer(screen, 123)
+    screen = pygame.display.set_mode([1000, 1000])
+    main_renderer = renderer.Renderer(screen)
     main_renderer.main_loop()
--- continuity/renderer.py
+++ continuity/renderer.py
@@ -1,41 +1,24 @@
 import pygame
 from dataclasses import dataclass
 
-class Camera:
-    ypos: float
-    xpos: float
-    scale: float
-    def __init__(self, xpos=0.0, ypos=0.0, scale=1):
-        self.xpos = ypos
-        self.ypos = ypos
-        self.scale = scale
-
-@dataclass
-class RendererObject:
-    surface: pygame.Surface
-    scale: float
-    
 class Renderer:
-    def __init__(self, screen, terrain):
-        self.camera = Camera()
-
+    def __init__(self, screen):
         self.screen = screen
+        self.screen_size = 1000
+        self.objects = []
         
-        self.terrain = RendererObject(pygame.Surface((1000,1000)), 1.0)
-        self.terrain.surface.fill((50, 100, 0))
+        self.objects.append(pygame.Surface((1000,1000)))
+        self.terrain = self.objects[0]
+        self.terrain.fill((50, 100, 0))
         
     def render_screen(self):
         # clear the screen
-        self.screen.fill((30,30,30))
+        self.terrain.fill((50, 100, 0))
 
-        if self.terrain.scale != self.camera.scale:
-            self.terrain.surface = pygame.transform.scale(self.terrain.surface, (1000 * self.camera.scale, 1000 * self.camera.scale))
-            self.terrain.scale = self.camera.scale
-        self.screen.blit(self.terrain.surface, pygame.Rect((0 + self.camera.xpos) * self.camera.scale, (0 + self.camera.ypos) * self.camera.scale, 1000 * self.camera.scale, 1000 * self.camera.scale))
+        pygame.draw.circle(self.terrain, (255, 0, 0), (100, 100), 10)
 
-        pygame.draw.circle(self.screen, (255, 0, 0), ((250 + self.camera.xpos) * self.camera.scale, (250 + self.camera.ypos) * self.camera.scale), 10 * self.camera.scale)
-
-
+        self.screen.blit(self.terrain, pygame.Rect((0, 0), (1000, 1000)))
+        
     def main_loop(self):
         running = True
     
@@ -45,19 +28,12 @@ class Renderer:
                     running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     x, y = pygame.mouse.get_pos()
-                    if event.button == 4:
-                        self.camera.scale *= 1.5
-                    elif event.button == 5:
-                        self.camera.scale /= 1.5
-                elif event.type == pygame.MOUSEMOTION and pygame.mouse.get_pressed()[2]:
-                    mouse_dx, mouse_dy = event.rel
-                    self.camera.xpos += mouse_dx / self.camera.scale
-                    self.camera.ypos += mouse_dy / self.camera.scale
                 
             self.render_screen()
             pygame.display.flip()
 
         pygame.quit()
+        return True
         
 def read_heightmap(path):
     pass
