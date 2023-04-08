34359bcb7af451e6d2ae9b6933c2a434b9291fc3
Andrei <andreisva2023@gmail.com>
2023-04-08 16:52:44 -0700

    touchup the neural network code

--- continuity/network.py
+++ continuity/network.py
@@ -1,4 +1,4 @@
-import numpy
+#import numpy
 from dataclasses import dataclass
 import random
 
@@ -13,7 +13,7 @@ class Neuron:
         self.value = value
         self.connections = connections
     def propagate(self, bias):
-        output_value = sum(map(lambda x : x["weight"] * x["neuron"].value)) + (bias[0] * bias[1])
+        output_value = sum(map(lambda x : x["weight"] * x["neuron"].value, self.connections)) + (bias[0] * bias[1])
         output_value = activation(output_value)
         self.value = output_value
 
@@ -29,24 +29,39 @@ class Brain:
 
         self.bias = [1, random.randint(1, 10)]
         layer = []
-
+        #print(self.network)
         for i in range(5):
-            connections = list(map(self.network[0], lambda x : {"weight": random.randint(1, 10), "neuron": x}))
+            connections = list(map(lambda x : {"weight": random.randint(-10, 10), "neuron": x}, self.network[0]))
             layer.append(Neuron(connections))
 
         self.network.append(layer)
 
         output_layer = []
         for i in range(4):
-            connections = list(map(self.network[-1], lambda x : {"weight": random.randint(1, 10), "neuron": x}))
+            connections = list(map(lambda x : {"weight": random.randint(-10, 10), "neuron": x}, self.network[-1]))
             output_layer.append(Neuron(connections))
 
         self.network.append(output_layer)
     def think(self, inputs):
         for i, neuron in enumerate(self.network[0]):
             neuron.value = inputs[i]
-        for layer_index in range(1, len(self.network) - 1):
+        for layer_index in range(1, len(self.network)):
             for neuron in self.network[layer_index]:
                 neuron.propagate(self.bias)
 
-        return tuple(map(self.network[-1], lambda x : x.value))
+        return tuple(map(lambda x : (1 if x.value == max(self.network[-1], key=lambda y : y.value) else 0), self.network[-1]))
+
+if __name__ == "__main__":
+
+    #i = [10, 20]
+    while True:
+        x = Brain()
+        y = [int(x) for x in input("> ").split()]
+        #y = [10, 20]
+        result = x.think(y)
+        #if result != (0, 0, 0, 0):
+        #    print(result)
+        print(list(map(lambda x : x.value, x.network[0])))
+        print(list(map(lambda x : x.value, x.network[1])))
+        print(list(map(lambda x : x.value, x.network[2])))
+        print(result)
